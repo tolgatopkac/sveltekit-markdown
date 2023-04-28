bd008439cdf12af4ae6195cc740b10336689e381
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-04-29 01:21:33 +0300

    🎨 style(app.css): add global CSS variables and styles 🔧 chore(config.ts): import dev environment variable from $app/environment 🎨 style(+layout.svelte): add layout component with header and footer 🎨 style(header.svelte): add header component with navigation links 🎨 style(footer.svelte): add footer component with title and year The changes add global CSS variables and styles to the app, which improves consistency and makes it easier to maintain. The layout component is added to the app, which includes a header and footer. The header component includes navigation links, and the footer component includes the title and year. The dev environment variable is imported from $app/environment to improve maintainability.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,5 @@
+import { dev } from "$app/environment";
+
 export const title = "Markdown Blog";
 export const description = "Sveltekit blog for exercise"
 export const url = dev ? 'http://localhost:5173' : 'vercel url'
