80d64a7ddd12c033dd0696f08f9f08e3b40f32a9
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-04-29 11:44:16 +0300

    🎨 style(app.html): add script to set color scheme based on localStorage 🚀 feat(theme.ts): add theme store and functions to toggle and set theme 🚀 feat(header.svelte): add toggle button to header 🚀 feat(toggle.svelte): add toggle button component The script in app.html sets the color scheme of the page based on the value of the 'color-scheme' key in localStorage. If the key is not set, it defaults to 'dark'. The theme.ts file exports a writable store for the theme, and functions to toggle and set the theme. The header.svelte file now imports the Toggle component and adds it to the header. The toggle.svelte file exports a button component that toggles the theme when clicked. The button uses the lucide-svelte icons for the sun and moon, and animates the icon and text when clicked.

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import * as config from '$lib/config'
+	import Toggle from './toggle.svelte'
 </script>
 
 <nav>
@@ -22,7 +23,7 @@
 	</ul>
 
 	<!-- Theme -->
-	<button>Toggle</button>
+	<Toggle />
 </nav>
 
 <style>
