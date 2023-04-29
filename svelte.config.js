d93cfeb01720250aaa5651fd4bc015fc19753651
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-04-29 11:05:50 +0300

    📦 chore(deps): add remark-toc, remark-unwrap-images, and rehype-slug to package.json ✨ feat(second-post.md): add table of contents to second post 🔧 refactor(svelte.config.js): add remark-toc, remark-unwrap-images, and rehype-slug to mdsvexOptions The dependencies remark-toc, remark-unwrap-images, and rehype-slug were added to package.json. The second post now has a table of contents. The svelte.config.js file was updated to include the remark-toc, remark-unwrap-images, and rehype-slug plugins in the mdsvexOptions.

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import { vitePreprocess } from '@sveltejs/kit/vite'
 
 import { escapeSvelte, mdsvex } from 'mdsvex'
 import shiki from 'shiki'
+import remarkUnwrapImages from 'remark-unwrap-images'
+import remarkToc from 'remark-toc'
+import rehypeSlug from 'rehype-slug'
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
@@ -16,7 +19,9 @@ const mdsvexOptions = {
 			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
 			return `{@html \`${html}\`}`
 		}
-	}
+	},
+	remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }]],
+	rehypePlugins: [rehypeSlug]
 }
 
 /** @type {import('@sveltejs/kit').Config} */
